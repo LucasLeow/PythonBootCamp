83a9546793099565c4764c81095ae0b72a65e5e9
LucasLeow <leowjianhoong@gmail.com>
2023-10-24 15:01:47 +0800

    Completed BlackJack game

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -1,5 +1,6 @@
 import random
 from blackjack_art import logo
+import os
 
 class BlackJack:
     pictures = ['J', 'Q', 'K']
@@ -20,6 +21,9 @@ class BlackJack:
             self.computer_deck.append(random.choice(self.deck))
             
     def compute_score(self):
+        self.player_score = 0
+        self.computer_score = 0
+
         for card in self.player_deck:
             if card in BlackJack.pictures:
                 self.player_score += 10
@@ -33,7 +37,7 @@ class BlackJack:
         for card in self.computer_deck:
             if card in BlackJack.pictures:
                 self.computer_score += 10
-            elif card == 'A' and (self.player_score + 11) <= 21:
+            elif card == 'A' and (self.computer_score + 11) <= 21:
                 self.computer_score += 11
             elif card == 'A':
                 self.computer_score += 1
@@ -54,29 +58,112 @@ class BlackJack:
 
 
     def print_score(self):
-        self.compute_score()
-        print(f'Your cards: [{self.player_deck[0]}, {self.player_deck[1]}], current score: {self.player_score}')
-        print(f'Computer cards: [{self.computer_deck[0]}, {self.computer_deck[1]}, computer score: {self.computer_score}]')
+        print(f'Your cards: {self.player_deck}, current score: {self.player_score}')
+        print(f'Computer cards: {self.computer_deck}, computer score: {self.computer_score}')
+
+    def check_status(self):
 
         self.check_blk_jck()
         self.check_ovr()
 
         if self.player_blk_jck and self.computer_blk_jck:
-            print("Both BlackJack! Draw")
+            print('Both BlackJack! Draw')
+            return 1
         elif self.player_blk_jck:
             print('Player BlackJack. Congratulations, You Won!')
+            return 1
         elif self.computer_blk_jck:
             print('Computer BlackJack. You Lost!')
             print(f'Computer cards: [{self.computer_deck[0]}, {self.computer_deck[1]}]')
+            return 1
+
+        if self.player_ovr and self.computer_ovr:
+            print('Both over 21! Draw')
+            return 1
+        elif self.player_ovr:
+            print('You exceeded 21. You Lost!')
+            return 1
+        elif self.computer_ovr:
+            print('Computer exceeded 21. You Won!')
+            return 1
+
+        return 0
+
+    def draw_card(self):
+        usr_choice = input("Draw a card?: 'y' or 'n' ")
+        if usr_choice == 'y':
+            self.player_deck.append(random.choice(self.deck))
+            return 1
+        else:
+            return 0
+
+    def compare_final_scores(self):
+        if self.player_score == self.computer_score:
+            print('Game Tie!')
+        elif self.player_score > self.computer_score:
+            print(f'You Won by {self.player_score - self.computer_score} points')
+        else:
+            print(f'You lost by {self.computer_score - self.player_score} points')
+
+        self.print_score()
+
+if __name__ == '__main__':
+
+    game_flag = True
+
+    while (game_flag):
+        play_again_flag = False
+        usr_choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n'")
+        if usr_choice == 'y':
+            print(logo)
+            game = BlackJack()
+            game.generate_deck()
+            game.compute_score()
+            game.print_score()
+
+            game_end = game.check_status()
+
+            print(game_end)
+            while not game_end:
+                usr_draw_card = game.draw_card()
+                if usr_draw_card:
+                    game.compute_score()
+                    game.print_score()
+                    game_end = game.check_status()
+                else:
+                    game.compare_final_scores()
+                    break
+
+            while not play_again_flag:
+                play_again = input("Play Again? 'y' or 'n' ")
+                if play_again == 'y':
+                    play_again_flag = True
+                    game_flag = True
+                    os.system('cls')
+
+                elif play_again == 'n':
+                    play_again_flag = True
+                    game_flag = False
+                    print('Have a nice day')
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'")
+                    continue
 
 
-usr_choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n'")
+        elif usr_choice == 'n':
+            print('Have a nice day.')
+            break
 
-if usr_choice == 'y':
-    print(logo)
-    game = BlackJack()
-    game.generate_deck()
-    game.print_score()
+        else:
+            print("Invalid input. Please enter 'y' or 'n' ")
+            continue
+
+
+
+
+
+
+
 
 
     
