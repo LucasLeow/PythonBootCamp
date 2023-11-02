b5870a2fe13b409e7a98fd5f228fa5e79b821fee
Lucas Leow <leowjianhoong@gmail.com>
2023-11-02 21:59:58 +0800

    completed true false quiz

--- 17_TrueFalseQuiz/main.py
+++ 17_TrueFalseQuiz/main.py
@@ -1,13 +1,32 @@
 from question_model import Question
 from data import question_data
 from quiz import Quiz
+import os
+from art import logo
 
 question_bank = list()
 for qns in question_data:
     question_bank.append(Question(qns['text'], qns['answer']))
 
-quiz_game = Quiz(question_bank)
-while(quiz_game.still_has_questions()):
-    correct = quiz_game.ask_question()
+
+play_again = True
+while play_again:
+    print(logo)
+    os.system('cls')
+    quiz_game = Quiz(question_bank)
+    while(quiz_game.still_has_questions()):
+        correct = quiz_game.ask_question()
+
+        if not correct:
+            while True:
+                usr_choice = input('Play again? (y or n): ')
+                if usr_choice.lower() == 'y':
+                    break
+                elif usr_choice.lower() == 'n':
+                    play_again = False
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'")
 
 
+print('Thank you for playing. Have a nice day!')
--- 17_TrueFalseQuiz/quiz.py
+++ 17_TrueFalseQuiz/quiz.py
@@ -1,3 +1,5 @@
+import random
+
 class Quiz:
     def __init__(self, qns_bank):
         self.question_number = 0
@@ -9,13 +11,14 @@ class Quiz:
 
     def ask_question(self):
         validation_flag = True
+        qns = random.choice(self.question_list)
         while validation_flag:
-            usr_answer = input(f"Q{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)? : ")
+            usr_answer = input(f"Q{self.question_number + 1}: {qns.text} (True/False)? : ")
             if usr_answer.lower() in ['true', 'false']:
                 break
             else:
                 print('Invalid input. Please enter true or false only')
-        correct = self.check_ans(usr_answer)
+        correct = self.check_ans(usr_answer, qns)
         if correct:
             self.question_number += 1
             print('You got it right!')
@@ -27,8 +30,8 @@ class Quiz:
             self.question_number = len(self.question_list)
             return False
 
-    def check_ans(self, usr_ans):
-        if usr_ans.lower() == self.question_list[self.question_number].answer.lower():
+    def check_ans(self, usr_ans, qns):
+        if usr_ans.lower() == qns.answer.lower():
             self.score += 1
             return True
         return False
