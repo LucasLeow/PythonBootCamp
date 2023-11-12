4ac9fd27816f7a92a967f7a9fa23ec58a844a39b
Lucas Leow <leowjianhoong@gmail.com>
2023-11-12 20:41:57 +0800

    implemented bounce logic

--- 22_PongGame/ball.py
+++ 22_PongGame/ball.py
@@ -1,12 +1,28 @@
 from turtle import Turtle
 
+#Constants
+BALL_MOVE_DIST = 10
+new_x = 0
+new_y = 0
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__(shape='circle')
         self.color('white')
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def wall_bounce(self):
+        self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
+
+
+
--- 22_PongGame/main.py
+++ 22_PongGame/main.py
@@ -7,6 +7,13 @@ import time
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+RIGHT_PADDLE_POS = 350
+LEFT_PADDLE_POS = -350
+
+WALL_TOLERANCE = 10
+PADDLE_TOLERANCE = 20
+
+
 
 if __name__ == '__main__':
     scn = Screen()
@@ -15,9 +22,10 @@ if __name__ == '__main__':
     scn.title('Pong')
     scn.tracer(n=0)
 
-    r_paddle = Paddle(350, 0)
-    l_paddle = Paddle(-350, 0)
+    r_paddle = Paddle(RIGHT_PADDLE_POS, 0)
+    l_paddle = Paddle(LEFT_PADDLE_POS, 0)
     b = Ball()
+    b.direction = 'right_up'
 
     scn.listen()
     scn.onkeypress(key="Up", fun=r_paddle.move_paddle_up)
@@ -25,10 +33,25 @@ if __name__ == '__main__':
     scn.onkeypress(key="w", fun=l_paddle.move_paddle_up)
     scn.onkeypress(key="s", fun=l_paddle.move_paddle_down)
 
+
     game_is_on = True
     while game_is_on:
         scn.update()
         b.move()
+        # Detect collision with walls
+        if (
+            b.ycor() >= (SCREEN_HEIGHT // 2) - WALL_TOLERANCE or
+                b.ycor() <= -(SCREEN_HEIGHT // 2) + WALL_TOLERANCE
+        ):
+            b.wall_bounce()
+
+        # Detect collision with paddle
+        if (
+                b.distance(r_paddle) < 50 and b.xcor() > RIGHT_PADDLE_POS - PADDLE_TOLERANCE or
+                b.distance(l_paddle) < 50 and b.xcor() < LEFT_PADDLE_POS + PADDLE_TOLERANCE
+        ):
+            b.paddle_bounce()
+
         time.sleep(0.1)
 
 
