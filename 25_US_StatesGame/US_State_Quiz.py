d1c6d2b827ea271220981055f9c13a6507c1fd73
Lucas Leow <leowjianhoong@gmail.com>
2023-11-16 11:28:57 +0800

    completed US State Game

--- 25_US_StatesGame/US_State_Quiz.py
+++ 25_US_StatesGame/US_State_Quiz.py
@@ -1,19 +1,51 @@
 import turtle
+import pandas as pd
 
 # Constants
 IMG_PATH = 'blank_states_img.gif'
 SCN_HEIGHT = 1000
 SCN_WIDTH = 1000
 
+correct_count = 0
+
 scn = turtle.Screen()
 scn.setup(width=SCN_WIDTH, height=SCN_HEIGHT)
 scn.title('US State Quiz')
-
 scn.addshape(IMG_PATH)
+
 t = turtle.shape(IMG_PATH)
+t = turtle.Turtle()
+t.hideturtle()
+
+df = pd.read_csv('50_states.csv')
+df['state'] = df['state'].str.lower()
+states = list(map(str.lower, df['state'].tolist()))
+print(states)
+
+def write_state(state, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.write(state)
 
 user_ans = scn.textinput(title='Guess the State',
-                         prompt="Enter a state name")
+                         prompt="Enter a state name").lower()
+
+while len(states) != 0:
+
+    if user_ans in states:
+        correct_count += 1
+        states.remove(user_ans)
+        x_cor = df[df['state'] == user_ans]['x'].values[0]
+        y_cor = df[df['state'] == user_ans]['y'].values[0]
+        write_state(user_ans.title(), x_cor, y_cor)
+
+        user_ans = scn.textinput(title=f'{correct_count}/{df.shape[0]} States Correct',
+                                 prompt="Enter another state name").lower()
+
+    else:
+        user_ans = scn.textinput(title=f'{correct_count}/{df.shape[0]} States Correct',
+                                 prompt="Enter another state name").lower()
+
 
 scn.exitonclick()
 
