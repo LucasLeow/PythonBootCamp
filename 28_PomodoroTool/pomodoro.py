55af9dd76f94a2bc3098bad26232306ea96b3371
Lucas Leow <leowjianhoong@gmail.com>
2023-11-18 13:07:12 +0800

    implemented check marks

--- 28_PomodoroTool/pomodoro.py
+++ 28_PomodoroTool/pomodoro.py
@@ -9,26 +9,31 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+sets = 0
 
-chk_mark = "✔"
+reps_chk_mark = "✔"
+sets_chk_mark = "✨"
+
+chk_marks = ""
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    global reps
+    global reps, sets
     reps += 1
 
     # work_time = WORK_MIN * 60
     # short_break_time = SHORT_BREAK_MIN * 60
     # long_break_time = LONG_BREAK_MIN * 60
 
-    work_time = 3
-    short_break_time = 2
-    long_break_time = 6
+    work_time = 1
+    short_break_time = 1
+    long_break_time = 1
 
     if reps % 8 == 0:
         timer_label.config(text='Long Break', fg=RED)
+        sets += 1
         count_down(seconds=long_break_time)
     elif reps % 2 == 0:
         timer_label.config(text='Short Break', fg=PINK)
@@ -39,6 +44,7 @@ def start_timer():
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(seconds):  # recursive call to countdown
+    global reps, sets, chk_marks
     mins, secs = divmod(seconds, 60)  # returns quotient (mins) & remainder (secs)
     text = '{:02d}:{:02d}'.format(mins, secs)
     canvas.itemconfig(timer_text, text=text)  # Update timer_text
@@ -46,6 +52,14 @@ def count_down(seconds):  # recursive call to countdown
         window.after(1000, count_down, seconds - 1)  # .after() calls function after some time
         # cannot use while loop because interferes with canvas mainloop()
     else:
+        if reps % 2 == 0:
+            if sets == 1:
+                chk_marks += sets_chk_mark
+                sets = 0
+                chk_mark_label.config(text=chk_marks)
+            else:
+                chk_marks += reps_chk_mark
+                chk_mark_label.config(text=chk_marks)
         start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -76,6 +90,6 @@ start_btn.grid(column=0, row=2)
 reset_btn.grid(column=2, row=2)
 
 # == checkmark ==
-chk_mark_label = Label(text=chk_mark, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, 'normal'))
+chk_mark_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, 'normal'))
 chk_mark_label.grid(column=1, row=3)
 window.mainloop()
