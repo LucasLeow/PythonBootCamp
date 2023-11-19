29c0ca8f26ea2198ed2e6932d84aa1a293424dab
Lucas Leow <leowjianhoong@gmail.com>
2023-11-19 16:47:55 +0800

    started on day 30 exception handling

---
+++ 30_ExceptionHandling/basic_exception_handling.py
@@ -0,0 +1,18 @@
+try:
+    file = open('a_file.txt')
+    a_dictionary = {"key": "value"}
+    print(a_dictionary['asdasd'])
+
+except FileNotFoundError:
+    file = open('a_file.txt', 'w')
+    file.write('something')
+    file.close()
+
+except KeyError as e_msg:
+    print(f'The key {e_msg} does not exist')
+
+else:
+    print('Program ran successfully')
+
+finally:
+    print('Program ending')
