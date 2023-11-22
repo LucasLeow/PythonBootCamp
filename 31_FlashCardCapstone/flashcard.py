da00ccae546ead8b284c2f34da246e903e532b5e
LucasLeow <leowjianhoong@gmail.com>
2023-11-22 15:18:33 +0800

    completed flashcard capstone

--- 31_FlashCardCapstone/flashcard.py
+++ 31_FlashCardCapstone/flashcard.py
@@ -16,29 +16,41 @@ WORDS_PATH = 'data/french_words.csv'
 
 # ---------------------------- GENERATE NEW FLASHCARD ------------------------------- #
 def generate_new_word():
-    another_pair = random.choice(words_list)
-    french_word = another_pair['French']
+    global flip_timer, random_pair
+    window.after_cancel(flip_timer)  # Cancels flip timer for every new card requested upon button press
+    random_pair = random.choice(words_list)
+    french_word = random_pair['French']
 
     canvas.itemconfig(card_canvas, image=card_bg)
-    canvas.itemconfig(title_text, text='French')
-    canvas.itemconfig(word_text, text=french_word)
+    canvas.itemconfig(title_text, text='French', fill='black')
+    canvas.itemconfig(word_text, text=french_word, fill='black')
 
-    window.after(3000, flip_card, another_pair)
+    flip_timer = window.after(3000, flip_card, random_pair)
 
 
-# ---------------------------- FLIP FLASHCARD ------------------------------- #
+def correct_btn():
+    words_list.remove(random_pair)
+    new_df = pd.DataFrame.from_records(words_list)
+    new_df.to_csv('words_to_learn.csv')
+    generate_new_word()
+
+
+# # ---------------------------- FLIP FLASHCARD ------------------------------- #
 def flip_card(word_pair):
     english_word = word_pair['English']
     canvas.itemconfig(card_canvas, image=card_after_bg)
-    canvas.itemconfig(title_text, text='English')
-    canvas.itemconfig(word_text, text=english_word)
+    canvas.itemconfig(title_text, text='English', fill='white')
+    canvas.itemconfig(word_text, text=english_word, fill='white')
 
 
 # ---------------------------- WORDS SETUP ------------------------------- #
-df = pd.read_csv(WORDS_PATH)
+try:
+    df = pd.read_csv('words_to_learn.csv')
+except FileNotFoundError:
+    df = pd.read_csv(WORDS_PATH)
+
 words_list = df.to_dict(orient='records')
 random_pair = random.choice(words_list)
-
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('French FlashCard')
@@ -62,11 +74,11 @@ cross_img = PhotoImage(file='images/wrong.png')  # 100 x 99
 right_img = PhotoImage(file='images/right.png')  # 100 x 100
 
 cross_btn = Button(image=cross_img, highlightthickness=0, command=generate_new_word)
-right_btn = Button(image=right_img, highlightthickness=0, command=generate_new_word)
+right_btn = Button(image=right_img, highlightthickness=0, command=correct_btn)
 
 cross_btn.grid(column=0, row=1)
 right_btn.grid(column=1, row=1)
 
-window.after(3000, flip_card, random_pair)
+flip_timer = window.after(3000, flip_card, random_pair)
 
 window.mainloop()
