ce37d42d8c5ee389d93d508c82ebf2e44cfc453a
Lucas Leow <leowjianhoong@gmail.com>
2023-11-23 14:13:14 +0800

    completed motivation monday

---
+++ 32_BirthdayEmailSender/motivation_monday.py
@@ -0,0 +1,34 @@
+import datetime as dt
+from creds import *
+import smtplib
+import random
+
+# Read all quotes
+with open('quotes.txt') as file:
+    all_lines = [row.strip('\n') for row in file.readlines()]
+    all_lines = [row.split('-') for row in all_lines]
+
+def get_quote():
+    random_quote = random.choice(all_lines)
+    return random_quote
+
+# == Email Mechanism ==
+day_of_week = dt.datetime.today().weekday()
+if day_of_week == 3:
+    r_quote = get_quote()
+    quote = r_quote[0]
+    author = r_quote[1]
+
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=pw)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_address,
+            msg=f'Subject: Motivation Monday \n\n {quote} \n\n - {author}'
+        )
+    print('email sent')
+
+
+
+
