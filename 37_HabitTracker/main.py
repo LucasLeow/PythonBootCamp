c169cbae3240791a178d61202f9719fa93b38f13
Lucas Leow <leowjianhoong@gmail.com>
2023-11-25 09:18:59 +0800

    completed graph creation endpoint

--- 37_HabitTracker/main.py
+++ 37_HabitTracker/main.py
@@ -1,11 +1,37 @@
 import requests
-from creds import user_params
+from creds import user_create_params, headers
 
 pixela_endpoint = 'https://pixe.la/v1/users'
-graph_creation_endpoint = f'{pixela_endpoint}/{user_create_params["username"]/graphs}'
+graph_creation_endpoint = f"{pixela_endpoint}/{user_create_params['username']}/graphs"
 
-# 1 create new user account
+# == 1 create new user account ==
+
 # res = requests.post(url=pixela_endpoint, json=user_create_params)
 # print(res.text)
 
-# 2 Create graph definition
+# == 2 Create graph definition ==
+'''
+graph req body: id | name | unit | type | color
+id - unique id for graph
+name - name of graph
+unit - unit of measure for activity
+type -  measure type ("float" or "int")
+color - shibafu(g), momiji(r), sora(blue), ichou(y), ajisai(p), kuro(blk)
+graph access: https://pixe.la/v1/users/lucasl96/graphs/graph1.html
+'''
+
+# graph_config = {
+#     "id": "graph1",
+#     "name": "coding",
+#     "unit": "km",
+#     "type": "float",
+#     "color": "sora"
+# }
+
+# res = requests.post(
+#     url=graph_creation_endpoint,
+#     json=graph_config,
+#     headers=headers
+# )
+
+# print(res.text)
