40cfd266adc2f25f82690f0b7eb251b4dc55e3e3
Lucas Leow <leowjianhoong@gmail.com>
2023-12-10 15:18:06 +0800

    completed blog capstone

--- 59_BlogCapstone/main.py
+++ 59_BlogCapstone/main.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 blog_data = requests.get(
     url='https://api.npoint.io/c97283d17102d10ad110'
 ).json()
-
+post_date = datetime.datetime.today()
 @app.route('/')
 def home():
-    return render_template('index.html', posts=blog_data, date=datetime.datetime.today())
+    return render_template('index.html', posts=blog_data, date=post_date)
 
 @app.route('/about')
 def about():
@@ -19,6 +19,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/post/<int:blog_id>')
+def post(blog_id):
+    for blog in blog_data:
+        if blog['id'] == blog_id:
+            return render_template('post.html', post=blog, date=post_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
