ab2ec741c3a55e8d66cd6a3d1610ded859568041
Lucas Leow <leowjianhoong@gmail.com>
2023-12-13 22:03:45 +0800

    implemented css + bootstrap + cafe data rendering

--- 62_CoffeeWifiProject/main.py
+++ 62_CoffeeWifiProject/main.py
@@ -4,24 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import csv
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+SECRET_KEY = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = SECRET_KEY
 Bootstrap5(app)
-
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
@@ -60,6 +48,7 @@ def cafes():
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
+        print(list_of_rows)
     return render_template('cafes.html', cafes=list_of_rows)
 
 
