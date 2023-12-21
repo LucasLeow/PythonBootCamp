4407127062eb79972170a5df435e771f487c98d5
Lucas Leow <leowjianhoong@gmail.com>
2023-12-21 21:52:47 +0800

    implemented user authentication

--- 68_FlaskUserAuthentication/main.py
+++ 68_FlaskUserAuthentication/main.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, jsonify, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
+SECRET_KEY = os.environ['SECRET_KEY']  # required by Flask-Login
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = SECRET_KEY
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
 db.init_app(app)
+
+# CONFIGURE FLASK LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
 # CREATE TABLE IN DB
@@ -18,8 +29,8 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
- 
- 
+
+
 with app.app_context():
     db.create_all()
 
@@ -31,7 +42,7 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST': # Register form submit
+    if request.method == 'POST':  # Register form submit
         user_password = request.form.get('password')
         hash_password = generate_password_hash(
             password=user_password,
@@ -39,35 +50,62 @@ def register():
             salt_length=8
         )
         new_user = User(
-            email = request.form.get('email'),
-            name = request.form.get('name'),
-            password = hash_password
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=hash_password
         )
 
         db.session.add(new_user)
         db.session.commit()
 
-        return render_template('secrets.html')
+        # Login user after registration & adding details to db
+        login_user(new_user)
+
+        return redirect(url_for('secrets'))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Get user
+        user = db.session.execute(
+            db.select(User).where(User.email == email)
+        ).scalar()
+
+        if not user:  # user not exist (None)
+            return jsonify(
+                response={
+                    'not found': 'User with that email was not found in database'
+                }
+            ), 404
+
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
+
+    else:  # GET Login page
+        return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name)  # current_user is keyword used by flask-login
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', path='files/cheat_sheet.pdf')
 
